fe1fd3e8d45d34bce03ff89ad9555eb169614ce1
OlehBabiak <oleg_bob@ukr.net>
2021-04-28 15:00:13 +0300

    edit react native app

---
+++ components/FlatList.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import User from "./User";
+
+
+export default function FlatLists(styles) {
+    const [items, setItems] = useState([])
+
+    const fetchData = async() => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+        const json = await response.json()
+        setItems(json)
+    }
+    fetchData()
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={items}
+                renderItem={({item})=>{
+                    console.log(item)
+                    return <User item={item}/>
+                }}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+    );
+}
+
