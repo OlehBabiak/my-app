6daca93e45e2c89d4c94497cb24489551a8a0413
OlehBabiak <oleg_bob@ukr.net>
2021-05-01 22:34:24 +0300

    create home-work3 branch

---
+++ components/Posts/PostsComponents.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {getPosts} from "../../src/api/API";
+import Post from "./Post";
+
+
+export default function PostsComponents(props) {
+    const {navigation} = props
+
+    let [posts, setPosts] = useState([])
+    async function fetchData() {
+        let posts = await getPosts()
+        setPosts([...posts])
+    }
+
+    useEffect(()=> {
+        fetchData()
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={posts}
+                renderItem={({item})=>{
+                    return <Post item={item} nav={navigation}/>
+                }}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#9a9c41',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
