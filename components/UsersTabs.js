cae196c490fc622a480801e874bebab389e6b9c5
OlehBabiak <oleg_bob@ukr.net>
2021-05-07 17:49:15 +0300

    edit home-work4 branch

--- components/UsersTabs.js
+++ components/UsersTabs.js
@@ -4,15 +4,18 @@ import UserDetailsComponent from "../components/Users/UserDetailsComponent";
 import PostsComponents from "../components/Users/Posts/PostsComponents"
 import AlbumsComponents from "../components/Users/Albums/AlbumsComponents"
 import TodosComponents from "../components/Users/Todos/TodosComponents"
+import { NavigationContainer } from '@react-navigation/native';
 const Tab = createBottomTabNavigator();
 
 export default function UsersTabs() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="UsersDetails" component={UserDetailsComponent} />
-            <Tab.Screen name="Posts" component={PostsComponents} />
-            <Tab.Screen name="Albums" component={AlbumsComponents} />
-            <Tab.Screen name="Todos" component={TodosComponents} />
-        </Tab.Navigator>
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="UsersDetails" component={UserDetailsComponent} />
+                <Tab.Screen name="Posts" component={PostsComponents} />
+                <Tab.Screen name="Albums" component={AlbumsComponents} />
+                <Tab.Screen name="Todos" component={TodosComponents} />
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
